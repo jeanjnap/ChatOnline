baf5437700438f8bd13f9ba1f0d9e3f2b0b464d9
JeanJNAP <jeanjnap@gmail.com>
2019-01-07 11:49:04 -0200

    Register Activity

---
+++ app/src/main/java/com/jeanjnap/chat/Activities/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.jeanjnap.chat.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.jeanjnap.chat.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView createAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(R.string.login);
+
+        createAccount = findViewById(R.id.createAcount);
+
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/jeanjnap/chat/Activities/RegisterActivity.java
@@ -0,0 +1,30 @@
+package com.jeanjnap.chat.Activities;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.jeanjnap.chat.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle(R.string.register);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
